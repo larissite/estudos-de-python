944798aa8087b99e6d8f2bfef44827f52bd67c8e
Larissa de Castro Azevedo <90583823+larissite@users.noreply.github.com>
2022-10-10 23:20:55 -0300

    deletando arquivos para corrigir a ordem dos desafios

--- curso-em-video/Mundo1/desafio02.py
+++
@@ -1,5 +0,0 @@
-dia = input("Qual o dia que você nasceu?")
-mes = input("Qual o mês (por extenso) que você nasceu?")
-ano = input("Qual o ano que você nasceu?")
-
-print("Você nasceu no dia", dia, "de", mes, "de", ano, ". Correto?")
--- curso-em-video/Mundo1/desafio04.py
+++
@@ -1,7 +0,0 @@
-# desafio 003 - aula 06
-
-valor1 = int(input('Digite um valor: '))
-valor2 = int(input('Digite outro valor: '))
-soma = valor1 + valor2
-
-print('A soma entre {} e {} é igual a {}'.format(valor1, valor2, soma))
