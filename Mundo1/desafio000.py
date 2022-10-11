c9d89c4d1e7a0ab82aa7f565733cf4c861690cef
Larissa de Castro Azevedo <90583823+larissite@users.noreply.github.com>
2022-10-10 23:27:25 -0300

    deletando para corrigir diretório

--- Mundo1/desafio000.py
+++
@@ -1,3 +0,0 @@
-nome = input("Qual é o seu nome?")
-print("Olá, {}!".format(nome), "Seja bem-vindo/a!")
-
--- Mundo1/desafio002.py
+++
@@ -1,3 +0,0 @@
-primeiro = input("Digite um número: ")
-segundo = input("Digite mais um número: ")
-print("A soma é", (int(primeiro) + int(segundo)), ".")
